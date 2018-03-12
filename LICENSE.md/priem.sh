596af9f4f38f8dd0e666010ef20af1251e639ce2
AraMek <37299654+AraMek@users.noreply.github.com>
2018-03-12 17:43:58 +0300

    Update priem.sh

--- LICENSE.md/priem.sh
+++ LICENSE.md/priem.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0  -v udpscr port=9000 caps='application/x-rtp,media(string)video, clock-rate(int)90000, encoding-name=(string)JPEG' ! rtpjpeg ! videoconvert ! autovideosink sync=false
+gst-launch-1.0  -v udpsrc port=9000 caps='application/x-rtp,media=(string)video, clock-rate=(int)90000, encoding-name=(string)JPEG' ! rtpjpegdepay !jpegdec! videoconvert ! autovideosink sync=false
